e07922c2ab5bf90048c34f81f832e66f3590356d
deepak.kumar <deepak@localhost>
2018-06-10 17:04:57 +0530

    Dock Boot adpp

--- src/main/java/javatechy/dockboot/DockBootApplication.java
+++ src/main/java/javatechy/dockboot/DockBootApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GoogleApplication {
+public class DockBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GoogleApplication.class, args);
+		SpringApplication.run(DockBootApplication.class, args);
 	}
 }
